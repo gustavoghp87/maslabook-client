c6eddd0318b6b7387b55d79f3c3ea9ba6cf8f7f6
Gustavo Hernán Perrupato <ghp.2120@gmail.com>
2021-03-03 11:35:54 -0300

    elimino start url

--- src/Components/Search/Input.tsx
+++ src/Components/Search/Input.tsx
@@ -20,9 +20,9 @@ export const Input = (props:any) => {
             <Col lg={6} style={{margin: mobile ? '0 auto 20px auto' : 'auto',
                 padding: mobile ? '0' : '0 10px 0 0'}}>
                 <div className="form-group" style={{maxWidth:'100%', margin:'auto'}}>
-                    <input className="form-control" type="text" placeholder="Palabras"
+                    <input className="form-control" type="text" placeholder="Palabras (máx. 7)"
                         style={{maxWidth:'300px', margin: mobile ? 'auto' : '0 0 0 auto'}}
-                        onChange={(e:any) => setWords(e.target.value)}
+                        onChange={(e:any) => setWords(e.target.value)} autoFocus
                     />
                 </div>
             </Col>
